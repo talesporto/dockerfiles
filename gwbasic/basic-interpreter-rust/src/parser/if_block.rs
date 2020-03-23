7477120503f280ba86e0ba7a198c0c596b64cda6
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-03-23 08:11:29 +0100

    parsing elseif and else

--- gwbasic/basic-interpreter-rust/src/parser/if_block.rs
+++ gwbasic/basic-interpreter-rust/src/parser/if_block.rs
@@ -23,24 +23,48 @@ pub struct IfBlock {
 impl<T: BufRead> Parser<T> {
     pub fn try_parse_if_block(&mut self) -> Result<Option<Statement>> {
         if self.buf_lexer.try_consume_word("IF")? {
-            self.buf_lexer.demand_whitespace()?;
-            let condition = self.demand_expression()?;
-            self.buf_lexer.demand_whitespace()?;
-            self.buf_lexer.demand_specific_word("THEN")?;
-            self.buf_lexer.demand_eol()?;
-            let block = self.parse_block()?;
+            // parse main if block
+            let if_block = self._demand_conditional_block()?;
+            // parse additional elseif blocks
+            let mut else_if_blocks: Vec<ConditionalBlock> = vec![];
+            while self.buf_lexer.try_consume_word("ELSEIF")? {
+                else_if_blocks.push(self._demand_conditional_block()?);
+            }
+            // parse else block
+            let else_block = if self.buf_lexer.try_consume_word("ELSE")? {
+                self.buf_lexer.demand_eol()?;
+                Some(self.parse_block()?)
+            } else {
+                None
+            };
+            // parse end if
             self.buf_lexer.demand_specific_word("END")?;
             self.buf_lexer.demand_whitespace()?;
             self.buf_lexer.demand_specific_word("IF")?;
             self.buf_lexer.demand_eol_or_eof()?;
             Ok(Some(Statement::IfBlock(IfBlock {
-                if_block: ConditionalBlock::new(condition, block),
-                else_if_blocks: vec![],
-                else_block: None,
+                if_block: if_block,
+                else_if_blocks: else_if_blocks,
+                else_block: else_block,
             })))
         } else {
             Ok(None)
         }
+    }
+
+    fn _demand_conditional_block(&mut self) -> Result<ConditionalBlock> {
+        let condition = self._demand_condition()?;
+        let block = self.parse_block()?;
+        Ok(ConditionalBlock::new(condition, block))
+    }
+
+    fn _demand_condition(&mut self) -> Result<Expression> {
+        self.buf_lexer.demand_whitespace()?;
+        let condition = self.demand_expression()?;
+        self.buf_lexer.demand_whitespace()?;
+        self.buf_lexer.demand_specific_word("THEN")?;
+        self.buf_lexer.demand_eol()?;
+        Ok(condition)
     }
 }
 
@@ -65,6 +89,143 @@ mod tests {
                 ),
                 else_if_blocks: vec![],
                 else_block: None
+            })
+        );
+    }
+
+    #[test]
+    fn test_if_else() {
+        let input = r#"IF X THEN
+    PRINT X
+ELSE
+    PRINT Y
+END IF"#;
+        let mut parser = Parser::from(input);
+        let if_block = parser.try_parse_if_block().unwrap().unwrap();
+        assert_eq!(
+            if_block,
+            Statement::IfBlock(IfBlock {
+                if_block: ConditionalBlock::new(
+                    Expression::variable_name_unqualified("X"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("X")]
+                    )]
+                ),
+                else_if_blocks: vec![],
+                else_block: Some(vec![Statement::sub_call(
+                    "PRINT",
+                    vec![Expression::variable_name_unqualified("Y")]
+                )])
+            })
+        );
+    }
+
+    #[test]
+    fn test_if_else_if() {
+        let input = r#"IF X THEN
+    PRINT X
+ELSEIF Y THEN
+    PRINT Y
+END IF"#;
+        let mut parser = Parser::from(input);
+        let if_block = parser.try_parse_if_block().unwrap().unwrap();
+        assert_eq!(
+            if_block,
+            Statement::IfBlock(IfBlock {
+                if_block: ConditionalBlock::new(
+                    Expression::variable_name_unqualified("X"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("X")]
+                    )]
+                ),
+                else_if_blocks: vec![ConditionalBlock::new(
+                    Expression::variable_name_unqualified("Y"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("Y")]
+                    )]
+                )],
+                else_block: None
+            })
+        );
+    }
+
+    #[test]
+    fn test_if_else_if_two_branches() {
+        let input = r#"IF X THEN
+    PRINT X
+ELSEIF Y THEN
+    PRINT Y
+ELSEIF Z THEN
+    PRINT Z
+END IF"#;
+        let mut parser = Parser::from(input);
+        let if_block = parser.try_parse_if_block().unwrap().unwrap();
+        assert_eq!(
+            if_block,
+            Statement::IfBlock(IfBlock {
+                if_block: ConditionalBlock::new(
+                    Expression::variable_name_unqualified("X"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("X")]
+                    )]
+                ),
+                else_if_blocks: vec![
+                    ConditionalBlock::new(
+                        Expression::variable_name_unqualified("Y"),
+                        vec![Statement::sub_call(
+                            "PRINT",
+                            vec![Expression::variable_name_unqualified("Y")]
+                        )]
+                    ),
+                    ConditionalBlock::new(
+                        Expression::variable_name_unqualified("Z"),
+                        vec![Statement::sub_call(
+                            "PRINT",
+                            vec![Expression::variable_name_unqualified("Z")]
+                        )]
+                    )
+                ],
+                else_block: None
+            })
+        );
+    }
+
+    #[test]
+    fn test_if_else_if_else() {
+        let input = r#"IF X THEN
+    PRINT X
+ELSEIF Y THEN
+    PRINT Y
+ELSE
+    PRINT Z
+END IF"#;
+        let mut parser = Parser::from(input);
+        let if_block = parser.try_parse_if_block().unwrap().unwrap();
+        assert_eq!(
+            if_block,
+            Statement::IfBlock(IfBlock {
+                if_block: ConditionalBlock::new(
+                    Expression::variable_name_unqualified("X"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("X")]
+                    )]
+                ),
+                else_if_blocks: vec![ConditionalBlock::new(
+                    Expression::variable_name_unqualified("Y"),
+                    vec![Statement::sub_call(
+                        "PRINT",
+                        vec![Expression::variable_name_unqualified("Y")]
+                    )]
+                )],
+                else_block: Some(vec![Statement::sub_call(
+                    "PRINT",
+                    vec![Expression::variable_name_unqualified("Z")]
+                )])
             })
         );
     }
